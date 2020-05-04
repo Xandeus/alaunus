21fbad91a805c4e865b883252ecce5949f6745ca
Xandeus <alexander.mckeever@gmail.com>
2020-05-03 19:13:38 -0500

    Make changes to run on rpi and add led controls

--- server/src/app.js
+++ server/src/app.js
@@ -5,18 +5,17 @@ const morgan = require('morgan')
 const { exec } = require("child_process");
 
 const app = express()
+let ledState = false
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-
-function executeCommand () {
-}
 
 app.post('/posts', (req, res) => {
     let result = 'No response';
     console.log('Request: ', req.body);
     const command = req.body.command;
     const stripAnimations = [
+	{ title: "POWER", description: "Turn the LEDS on and off", state: ledState }, 
         { title: "rainbow", description: "Colorful rainbow" },
         { title: "colorful", description: "It's all colorful" }
     ];
@@ -40,12 +39,28 @@ app.post('/posts', (req, res) => {
 app.post('/toggles', (req, res) => {
     const toggleObj = req.body;
     console.log('Request: ', req.body);
+    let command = 'echo no actions taken';
     if (toggleObj.name === 'powerToggle' && toggleObj.state) {
-        console.log('Turning LEDS on');
+	command = './../send_command.sh';
+	ledState = true;
+    	console.log('Turning LEDS on');
     } else if (toggleObj.name === 'powerToggle') {
+	ledState = false;
+	command = './../send_stop.sh';
         console.log('Turning LEDS off');
     }
-    res.send();
+    exec(command, (error, stdout, stderr) => {
+	if (error) {
+	    console.log(`error: ${error.message}`);
+	    return;
+	}
+	if (stderr) {
+	    console.log(`stderr: ${stderr}`);
+	    return;
+	}
+	console.log(`stdout: ${stdout}`);
+	res.send('exectued command');
+    });
 })
 
 
