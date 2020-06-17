5baaf01d32cafea3c0c2981438ee7d4c5fab6638
Alex McKeever <alexander.mckeever@gmail.com>
2020-06-17 16:55:58 -0500

    Reduce amount of printing

--- client_pi.py
+++ client_pi.py
@@ -11,12 +11,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((pi_ip, 8268))
 
 for line in sys.stdin:
-    broke_up_line = line.split(";")
-    broke_up_lines = line.splitlines()
-    print(len(broke_up_line))
-    print(len(broke_up_lines))
-    print(broke_up_lines)
-    client_socket.sendall(line.encode("UTF-8"))
+    client_socket.send(line.encode("UTF-8"))
 
 #send disconnect message
 dmsg = "disconnect"
