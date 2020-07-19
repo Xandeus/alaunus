a2b408cba2eb873bfbbd8079c3de10a257b4a014
Xandeus <alexander.mckeever@gmail.com>
2020-07-19 14:23:07 -0500

    Fix class issues

--- led_animations.py
+++ led_animations.py
@@ -1,7 +1,21 @@
 from led_animation import LEDAnimation
+from neopixel import *
+import time
+# LED strip configuration:
+LED_COUNT      = 240     # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Intialize the library (must be called once before other functions).
+strip.begin()
 # Define functions which animate LEDs in various ways.
-test = LEDAnimation('colorWipe', colorWipe)
-def colorWipe(strip, color, wait_ms=50):
+def colorWipe(color, wait_ms=50):
     """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
@@ -122,3 +136,7 @@ def fade(strip, cycles, wait_ms=5):
         dRedVal = random.randint(0,255)
         dGreenVal = random.randint(0,255)
         dBlueVal = random.randint(0,255)
+
+test = LEDAnimation('colorWipe', colorWipe)
+print(test.name)
+test.run(Color(255, 0, 0))
